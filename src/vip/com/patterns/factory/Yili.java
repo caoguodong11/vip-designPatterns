fe6a6254d987241326ba8cbb8b1471615370680f
caoguodong <985572176@qq.com>
2018-03-16 01:13:38 +0800

    simple factory

---
+++ src/vip/com/patterns/factory/Yili.java
@@ -0,0 +1,10 @@
+package vip.com.patterns.factory;
+
+import vip.com.patterns.factory.Milk;
+
+public class Yili implements Milk {
+    @Override
+    public String getName() {
+        return "伊利";
+    }
+}
